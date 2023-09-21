4b1380b49a1efb33782079add9fc77ee9695815d
Diego Garcia Padilla <gpdiego370@outlook.com>
2023-09-21 11:03:26 -0600

    Add algorithm complexity

--- headers/Sorts.h
+++ headers/Sorts.h
@@ -21,11 +21,11 @@ template <class T>
 class Sorts
 {
 private:
-    void swap(T *, T *);                    // Swap function
-    void mergeSort(vector<T> &, int, int);  // Merge sort algorithm
-    void merge(vector<T> &, int, int, int); // Merge two subarrays
+    void swap(T *, T *);                    // Swap function. Complexity: O(1)
+    void mergeSort(vector<T> &, int, int);  // Merge sort algorithm. Complexity: O(n log n)
+    void merge(vector<T> &, int, int, int); // Merge two subarrays. Complexity: O(n)
 public:
-    void sort(vector<T> &); // Sorts a vector with the merge sort algorithm
+    void sort(vector<T> &); // Sorts a vector with the merge sort algorithm. Complexity: O(n log n)
 };
 
 #endif /* SORTS_H */
